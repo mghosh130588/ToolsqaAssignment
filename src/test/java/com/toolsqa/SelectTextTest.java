22494f567ac1d38bc22049d22750e03a234388a7
mghosh130588 <mousumi.nit@gmail.com>
2022-05-02 00:01:52 +0100

    thirs commit

--- src/test/java/com/toolsqa/SelectTextTest.java
+++ src/test/java/com/toolsqa/SelectTextTest.java
@@ -1,5 +1,7 @@
 package com.toolsqa;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -21,18 +23,20 @@ public class SelectTextTest extends TestBase {
     public SelectablePage sc;
     public Drropable dr;
     public WebDriver driver;
-
+    public  static Logger log = LogManager.getLogger(TestBase.class.getName());
     @BeforeTest
     public void openBrowser() throws IOException {
         this.driver = initializedriver();
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
         driver.manage().window().maximize();
+        log.info("Browser initiated");
 
     }
     @BeforeClass
-    public void clickonInteractions(){
-        driver.get("https://demoqa.com/");
-        System.out.println("link opened");
+    public void clickonInteractions() throws IOException {
+        driver.get(getPropertyValue("url"));
+        log.info("link opened");
         LandingPage lp = new LandingPage(driver);
         lp.clickOnInteractions();
     }
@@ -41,33 +45,40 @@ public class SelectTextTest extends TestBase {
     public void verifySelectPageOpen(){
 
         sc = new SelectablePage(driver, "Selectable");
+        log.info("Selectable page is opened");
     }
 
     @Test(priority =1)
     public void SelectLinks(){
 
         sc.clickOnAllListItems();
+        log.info("All links are selected");
     }
 
     @Test(priority =2)
     public void VerifyDroppable(){
     dr = new Drropable(driver,"Droppable");
+    log.info("Droppable page is opened");
     }
     @Test(priority =3)
     public void simpledragdrop() throws InterruptedException {
         dr.dragdropsimple();
+        log.info("Simple drag and drop is completed");
     }
     @Test(priority =4)
     public void AcceptNotAcceptdragdrop() throws InterruptedException {
         dr.AcceptNotAcceptdragdrop();
+        log.info("Accept not accept drag drop is completed");
     }
 
     @AfterMethod
     public void aftertestmethod(){
         driver.navigate().refresh();
+        log.info("page refreshed");
     }
     @AfterTest
     public void closebrowser(){
         driver.quit();
+        log.info("Browser closed");
     }
 }
--- target/classes/TestBase.java
+++ target/classes/TestBase.java
@@ -1,10 +1,15 @@
 package resources;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -12,12 +17,11 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
     public WebDriver driver;
     public String driverpath = "//Users//mousumighosh//Selenium//Driver//";
+    Properties prop;
+
     public WebDriver initializedriver() throws IOException {
 
-        Properties prop = new Properties();
-        FileInputStream fs = new FileInputStream("./src/main/java/resources/data.properties");
-        prop.load(fs);
-        String browsername = prop.getProperty("browser");
+        String browsername = getPropertyValue("browser");
         if(browsername.equalsIgnoreCase("chrome"))
         {
             System.setProperty("webdriver.chrome.driver",driverpath+"chromedriver");
@@ -31,5 +35,21 @@ public class TestBase {
         }
         return driver;
 
+    }
+
+    public String getPropertyValue(String PropertyName) throws IOException {
+         prop = new Properties();
+        FileInputStream fs = new FileInputStream("./src/main/java/resources/data.properties");
+        prop.load(fs);
+       return prop.getProperty(PropertyName);
+
+    }
+
+    public String takeScreenshot(String Testcasename, String Destination,WebDriver driver) throws IOException {
+        TakesScreenshot scr = (TakesScreenshot)driver;
+        File source = scr.getScreenshotAs(OutputType.FILE);
+        String FileDestination = Destination + Testcasename+".png";
+        FileUtils.copyFile(source,new File(FileDestination));
+        return FileDestination;
     }
 }
